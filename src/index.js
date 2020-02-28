a37320b14520cea48bd303c87ea353106b653045
Colin McNamara <coolinmc6@gmail.com>
2020-02-28 11:43:17 -0500

    my tweaks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {
-  qdtEnigma, qdtCompose, QdtTable, //eslint-disable-line
+  qdtEnigma, qdtCompose, QdtTable, QdtButton, //eslint-disable-line
 } from 'qdt-components';
+// import React from 'react';
 
 const config = {
   host: 'sense-demo.qlik.com',
@@ -10,37 +11,66 @@ const config = {
   appId: '372cbc85-f7fb-4db6-a620-9a5367845dce',
 };
 
+// const myButton = ({ label, type }) => ( //eslint-disable-line
+//   <QdtButton //eslint-disable-line
+//     label={label}
+//     type={type}
+//   />
+// );
+
+// https://sense-demo-staging.qlik.com/windows/sense/app/372cbc85-f7fb-4db6-a620-9a5367845dce/overview
+
 qdtEnigma(config).then((app) => {  //eslint-disable-line
-  const q = qdtCompose({
-    element: document.querySelector('.object'),
+  // qdtCompose({
+  //   element: document.querySelector('.object'),
+  //   component: QdtTable,
+  //   options: {},
+  //   app,
+  //   properties: {
+  //     qHyperCubeDef: {
+  //       qDimensions: [{
+  //         qDef: { qFieldDefs: ['[Product Group Desc]'] },
+  //       }],
+  //       qMeasures: [{
+  //         qDef: { qDef: 'Sum([Sales Price])' },
+  //       }],
+  //     },
+  //   },
+  // });
+
+  qdtCompose({
+    element: document.querySelector('.cm-object-1'),
     component: QdtTable,
     options: {},
     app,
     properties: {
       qHyperCubeDef: {
         qDimensions: [{
-          qDef: { qFieldDefs: ['[Product Group Desc]'] },
+          qDef: { qFieldDefs: ['[Month]'] },
         }],
         qMeasures: [{
-          qDef: { qDef: 'Sum([Sales Price])' },
+          qDef: { qDef: 'Sum([YTD Sales Amount])' },
         }],
       },
     },
   });
-  setTimeout(() => {
-    console.log('running update');
-    q.update({
-      element: document.querySelector('.object'),
-      properties: {
-        qHyperCubeDef: {
-          qDimensions: [{
-            qDef: { qFieldDefs: ['[Sales Rep Name]'] },
-          }],
-        },
-      },
+
+  qdtCompose({
+    element: document.querySelector('.cm-object-2'),
+    component: QdtButton,
+    options: { label: 'Clear All', type: 'clear' },
+    app,
+    properties: {},
+  });
+
+  app.createSessionObject({ qInfo: { qType: 'fields' }, qFieldListDef: {}, qStuff: {} }).then((res) => {
+    res.getLayout().then((layout) => {
+      console.log(layout);
+      const fields = layout.qFieldList.qItems.reduce((acc, obj) => {
+        acc[obj.qName] = obj;
+        return acc;
+      }, {});
+      console.log(fields);
     });
-  }, 5000);
-  setTimeout(() => {
-    q.destroy();
-  }, 10000);
+  });
 });
